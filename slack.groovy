c9f13c2c4bf2c7f0b8dc62e13d289a1a59f2cf88
Bartleonid <bartleonid@hotmail.com>
2019-12-16 16:22:55 +0000

    add slack notification
    Jenkins build settings:
    Post-build Actions
    Groovy postbuild
    Use groovy sandbox

---
+++ slack.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/env groovy
+void log(message) {
+  manager.listener.logger.println(message)
+}
+def build_url = manager.envVars['BUILD_URL']
+def job_name = manager.envVars['JOB_NAME']
+def build_number = manager.envVars['BUILD_NUMBER']
+log(build_url)
+
+def buildResult = manager.getResult()
+  if ( buildResult == "SUCCESS" ) {
+   def request = ["curl", "-k", "-X", "POST", "-H", "Content-Type: application/json", "--data", "{\"text\":\"Job: ${job_name} #${build_number} - SUCCESS ${build_url}\"}", "https://hooks.slack.com/services/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"].execute().text
+  }
+  else if( buildResult == "FAILURE" ) { 
+def request = ["curl", "-k", "-X", "POST", "-H", "Content-Type: application/json", "--data", "{\"text\":\"Job: ${job_name} #${build_number} - FAILURE ${build_url}\" }", "https://hooks.slack.com/services/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"].execute().text
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+def request = ["curl", "-k", "-X", "POST", "-H", "Content-Type: application/json", "--data", "{\"text\":\"Job: ${job_name} #${build_number} - UNSTABLE ${build_url}\"}", "https://hooks.slack.com/services/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"].execute().text
+  }
+  else {
+def request = ["curl", "-k", "-X", "POST", "-H", "Content-Type: application/json", "--data", "{\"text\":\"Job: ${job_name} #${build_number} - result was unclear ${build_url}\"}", "https://hooks.slack.com/services/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"].execute().text
+  }
